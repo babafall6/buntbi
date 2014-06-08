d69a3caabf8465e0add827f82c26610313e5a82b
Papa Baba Fall <babafall@soon.pro>
2014-06-08 16:16:44 +0000

    Structure de la table visite modifiee

---
+++ bd/visite.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 5.5.29, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: buntbi
+-- ------------------------------------------------------
+-- Server version	5.5.29-0ubuntu0.12.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `visite`
+--
+
+DROP TABLE IF EXISTS `visite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `visite` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_entree` datetime DEFAULT NULL,
+  `date_sortie` datetime DEFAULT NULL,
+  `id_visiteur` int(11) NOT NULL,
+  `created_by` varchar(30) NOT NULL,
+  `modified_by` varchar(30) NOT NULL,
+  `id_vigile` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_visiteur_idx` (`id_visiteur`),
+  KEY `id_vigile_idx` (`id_vigile`),
+  CONSTRAINT `id_vigile` FOREIGN KEY (`id_vigile`) REFERENCES `vigile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_visiteur` FOREIGN KEY (`id_visiteur`) REFERENCES `visiteur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `visite`
+--
+
+LOCK TABLES `visite` WRITE;
+/*!40000 ALTER TABLE `visite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `visite` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-06-08 16:14:44
